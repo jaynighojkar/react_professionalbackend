aca9c35d00c27adaf32d75900b75cfb5ff385663
Jay Nighojkar <janighoj@microsoft.com>
2020-12-07 17:32:22 +0530

    third commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+class MainClass {
+    static main() {
+        // tslint:disable-next-line:no-console
+        console.info("Welcome to the World of MERN!");
+    }
+}
+MainClass.main();
---
+++ index.ts
@@ -0,0 +1,14 @@
+class MainClass {​​​​
+
+    static main(): void {​​​​
+
+        // tslint:disable-next-line:no-console
+        console.info("Welcome to the World of MERN!");
+
+    }​​​​
+
+}​​​​
+
+
+
+MainClass.main();
